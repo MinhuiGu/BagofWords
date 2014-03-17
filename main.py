34609462d7c4f7b381a82a128f9c4dd6850d645a
Han Yan <enthalpy-yan@users.noreply.github.com>
2014-03-17 11:46:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from vq import *
+from optparse import OptionParser
+import os
+
+class OptException(Exception):
+    pass
 
 if __name__ == '__main__':
-    pic_folder = "../Graz02TranValidationTest/Train/bike/"
-    K = 25
-    my_code_book = code_book(pic_folder, K)
+    parser = OptionParser()
+    parser.add_option("-s", "--source", dest="folder_path",\
+                      help="the path of folder that contains images file.")
+    parser.add_option("-f", "--file", type="string", dest="filename", \
+                      help="the target image file.")
 
-    test_pic = "../Graz02TranValidationTest/Test/bike/bike_166.bmp"
-    hist = quatization(test_pic, my_code_book)
+    (options, args) = parser.parse_args()
+
+    if not options.folder_path:
+        raise OptException("Please input folder's path.")
+    else:
+        if not os.path.isdir(options.folder_path):
+            raise OptException('Invalid path.')
+
+    if not options.filename:
+        raise OptException("Please input file's path.")
+    else:
+        if not os.path.exists(options.filename):
+            raise OptException('Invalid file.')
+
+    K = 10
+    my_code_book = code_book(options.folder_path, K)
+    hist = hard_quatization(options.filename, my_code_book)
     print hist
